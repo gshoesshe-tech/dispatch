2e1037b462b1f9a27a8a67bcca36d77bea697125
gshoesshe-tech <gshoesshe@gmail.com>
2026-09-19 10:27:34 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public connection settings only. NEVER paste a service_role or secret key here.
 window.TRACKER_CONFIG = {
-  supabaseUrl: 'https://YOUR_PROJECT.supabase.co',
-  supabasePublishableKey: 'YOUR_PUBLISHABLE_OR_ANON_KEY'
+  supabaseUrl: 'https://votyujnrtezicuiejitn.supabase.co/',
+  supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdHl1am5ydGV6aWN1aWVqaXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MTU4ODcsImV4cCI6MjEwMTA5MTg4N30.0WNxal5FoHghIvjRf73LVFMPIwoe_jXoFYRtwn8NmH8'
 };
